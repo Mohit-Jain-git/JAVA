0059823e7e494b32bee45c0bad251b0725451543
Mohit-Jain-git <mohitjainmj2000@gmail.com>
2021-10-01 22:01:22 +0530

    FirstClassPattern

---
+++ pattern.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class pattern {
+  public static void main(String args[])
+  {
+    Scanner scan = new Scanner(System.in);
+    int m = scan.nextInt();
+    int n = scan.nextInt();
+    for(int i=0;i<m;i++)
+    {
+       for(int j=0;j<n;j++)
+       {
+         System.out.print("*");
+       }
+       System.out.println();
+    }
+  }
+   
+     
+}
