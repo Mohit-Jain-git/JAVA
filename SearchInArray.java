1ad41b28d005ffdb4d51f10fa5a86a521a0f293e
Mohit-Jain-git <mohitjainmj2000@gmail.com>
2021-11-11 11:14:42 +0530

    SearchinArray

--- SearchInArray.java
+++ SearchInArray.java
@@ -1,30 +1,27 @@
 import java.util.*;
 
 public class SearchInArray {
-  
-  public static void main(String args[])
-  {
+
+  public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
     int size = sc.nextInt();
     int i;
     int arr[] = new int[size];
-    for(i=0;i<size;i++)
-    {
+    for (i = 0; i < size; i++) {
       arr[i] = sc.nextInt();
     }
     System.out.println("Enter a number to Search : ");
     int x = sc.nextInt();
-    int flag=0;
-    for(i=0;i<size;i++)
-    {
+    int flag = 0;
+    for (i = 0; i < size; i++) {
       if (arr[i] == x) {
         flag = 1;
         break;
       }
     }
-    if(flag==1)
+    if (flag == 1)
       System.out.println("Element found at index " + i);
-      else
+    else
       System.out.println("Element not present in array.");
   }
 }
